cfdb83e868c7c86335936955d40d4f4deb904fc3
Felipe Teles <felipersteles@gmail.com>
2024-07-08 10:51:58 -0300

    Initial Commit

---
+++ cnn-from-scratch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cnn-from-scratch
+//
+//  Created by Felipe Teles on 08/07/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
